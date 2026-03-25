edc54e08dd4ddd6126ac363bc117653fe61b79f4
marcuscastelo <marcuscastelo@usp.br>
2026-03-25 18:57:31 -0300

    feat: enhance JSX rules and improve location usage messages in ESLint configuration

--- packages/eslint-config/solid.js
+++ packages/eslint-config/solid.js
@@ -103,15 +103,15 @@ export default [
     },
   },
 
-  // JSX rules (genéricas)
-  // TODO: aplicar quando tiver os rules movidos
+  // JSX rules (generic)
+  // TODO: enable when moved to shared plugin
   // {
-  // 	files: ["**/*.tsx"],
-  // 	rules: {
-  // 		"platform/no-jsx-short-circuit": "error",
-  // 		"platform/no-jsx-ternary": "error",
-  // 		"platform/no-iife-in-jsx": "warn",
-  // 	},
+  //   files: ["**/*.tsx"],
+  //   rules: {
+  //     "platform/no-jsx-short-circuit": "error",
+  //     "platform/no-jsx-ternary": "error",
+  //     "platform/no-iife-in-jsx": "warn",
+  //   },
   // },
 
   {
@@ -122,12 +122,55 @@ export default [
         {
           selector:
             "AssignmentExpression[left.type='MemberExpression'][left.object.name='location'][left.property.name='href']",
-          message: "Use router helpers instead of location.href",
+          message:
+            "Use router navigation helpers for internal routes. location.href assignment is forbidden.",
         },
         {
           selector:
             "MemberExpression[object.name='window'][property.name='location']",
-          message: "Use router helpers instead of window.location",
+          message:
+            "window.location usage is forbidden in app code. Use router navigation and helper utilities.",
+        },
+        {
+          selector:
+            "MemberExpression[object.name='document'][property.name='location']",
+          message:
+            "document.location usage is forbidden in app code. Use router navigation and helper utilities.",
+        },
+        {
+          selector:
+            "AssignmentExpression[left.type='MemberExpression'][left.object.type='MemberExpression'][left.object.object.name='window'][left.object.property.name='location'][left.property.name='href']",
+          message:
+            "Use router navigation helpers for internal routes. window.location.href assignment is forbidden.",
+        },
+        {
+          selector:
+            "CallExpression[callee.type='MemberExpression'][callee.object.name='location'][callee.property.name=/^(assign|replace|reload)$/]",
+          message:
+            "Use router navigation + targeted refetch/reconcile. location.assign/replace/reload are forbidden.",
+        },
+        {
+          selector:
+            "CallExpression[callee.type='MemberExpression'][callee.object.type='MemberExpression'][callee.object.object.name='window'][callee.object.property.name='location'][callee.property.name=/^(assign|replace|reload)$/]",
+          message:
+            "Use router navigation + targeted refetch/reconcile. window.location.assign/replace/reload are forbidden.",
+        },
+        {
+          selector:
+            "CallExpression[callee.type='MemberExpression'][callee.object.name='window'][callee.property.name='navigate']",
+          message: "Use router navigation helpers. window.navigate is forbidden.",
+        },
+        {
+          selector:
+            "CallExpression[callee.type='MemberExpression'][callee.object.name='history'][callee.property.name='pushState']",
+          message:
+            "Manual history.pushState is forbidden in app code. Use router navigation helpers.",
+        },
+        {
+          selector:
+            "CallExpression[callee.type='MemberExpression'][callee.object.type='MemberExpression'][callee.object.object.name='window'][callee.object.property.name='history'][callee.property.name='pushState']",
+          message:
+            "Manual window.history.pushState is forbidden in app code. Use router navigation helpers.",
         },
       ],
     },
@@ -140,7 +183,20 @@ export default [
         "error",
         {
           selector: "ExportNamedDeclaration > FunctionDeclaration",
-          message: "*.vm.ts must not export functions",
+          message:
+            "*.vm.ts files must export shape/type/constants only, not behavioral functions.",
+        },
+        {
+          selector:
+            "ExportNamedDeclaration VariableDeclaration > VariableDeclarator[init.type='ArrowFunctionExpression']",
+          message:
+            "*.vm.ts files must not export behavioral arrow functions.",
+        },
+        {
+          selector:
+            "ExportNamedDeclaration VariableDeclaration > VariableDeclarator[init.type='FunctionExpression']",
+          message:
+            "*.vm.ts files must not export behavioral function expressions.",
         },
       ],
     },
