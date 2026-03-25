de5b85e00debab6c6ba51a4d775c43d6a6cd583f
marcuscastelo <marcuscastelo@usp.br>
2026-03-25 14:57:36 -0300

    feat: enhance ESLint configuration for TypeScript and Solid integration

--- packages/eslint-config/solid.js
+++ packages/eslint-config/solid.js
@@ -1,9 +1,70 @@
-import platformPlugin from '@marcuscastelo/eslint-plugin'
+import platformPlugin from "@marcuscastelo/eslint-plugin";
+import solid from "eslint-plugin-solid/configs/typescript";
+import * as tsParser from "@typescript-eslint/parser";
 
 export default [
-  {
-    plugins: {
-      platform: platformPlugin
-    }
-  }
-]
+	{
+		plugins: {
+			platform: platformPlugin,
+		},
+	},
+
+	{
+		files: ["**/*.{ts,tsx}"],
+		...solid,
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				project: "tsconfig.json",
+			},
+		},
+		rules: {
+			...(solid.rules ?? {}),
+			"solid/components-return-once": "error",
+		},
+	},
+
+	// JSX rules (genéricas)
+	{
+		files: ["**/*.tsx"],
+		rules: {
+			"platform/no-jsx-short-circuit": "error",
+			"platform/no-jsx-ternary": "error",
+			"platform/no-iife-in-jsx": "warn",
+		},
+	},
+
+	// Navegação imperativa proibida (genérico)
+	{
+		files: ["**/*.{ts,tsx}"],
+		rules: {
+			"no-restricted-syntax": [
+				"error",
+				{
+					selector:
+						"AssignmentExpression[left.type='MemberExpression'][left.object.name='location'][left.property.name='href']",
+					message: "Use router helpers instead of location.href",
+				},
+				{
+					selector:
+						"MemberExpression[object.name='window'][property.name='location']",
+					message: "Use router helpers instead of window.location",
+				},
+			],
+		},
+	},
+
+	// ViewModel = shape only
+	{
+		files: ["**/*.vm.ts"],
+		rules: {
+			"no-restricted-syntax": [
+				"error",
+				{
+					selector: "ExportNamedDeclaration > FunctionDeclaration",
+					message: "*.vm.ts must not export functions",
+				},
+			],
+		},
+	},
+];
